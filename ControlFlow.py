175152afea9342191da5274a9d0175d4eefb9b9c
Brad Lange <blange@gulllakecs.org>
2020-01-07 08:18:14 -0500

    Update Running Total
    Users can now enter how many numbers they wish to sum up

--- ControlFlow.py
+++ ControlFlow.py
@@ -1,15 +1,17 @@
 """
 Programmer: Mr. Lange
 Date: 1.6.20
-Program: Running Total
+Program: Running Total, Part II
 
 This program asks users for five numbers
 It then sums up the numbers
 """
 
 sum = 0
+how_many_numbers = int(input("\nHow many numbers would you like to sum up: "))
+print("")
 
-for i in range(5):
+for i in range(how_many_numbers):
     enter_a_number = int(input("Enter a number: "))
     sum = sum + enter_a_number
 
