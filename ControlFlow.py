4e4e072f34b79a7c8dedaaba1aa47c0eca582fb6
Brad Lange <blange@gulllakecs.org>
2020-01-09 07:55:16 -0500

    Add Round & Updated Format

--- ControlFlow.py
+++ ControlFlow.py
@@ -27,11 +27,13 @@ This program asks users how many tests they wish to average
 """
 
 total = 0
-how_many_tests = int(input("How many tests would you like to average: "))
+how_many_tests = int(input("\nHow many tests would you like to average: "))
 print("")
 
 for i in range(how_many_tests):
-    enter_a_score = int(input("Enter a score: "))
+    enter_a_score = float(input("Enter a score: "))
     total = total + enter_a_score
 
 average = total / how_many_tests
+
+print("\nAverage: " + str(round(average, 2)))
